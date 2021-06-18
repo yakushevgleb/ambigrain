0d315c4d5299f9c9011a4ce7c5e09bbf70b299e6
yakushevgleb <yakushev.gleb.k@gmail.com>
2021-06-18 03:05:26 +0300

    Added async calls and fixed rotation

--- engine_core.js
+++ engine_core.js
@@ -40,63 +40,85 @@ const addRemovePoint = (pointsCount) => {
 
 const shuffleMovement = () => {
   for (let index = 0; index < coordsArray.length; index++) {
-    let { x, y, ...other } = coordsArray[index];
-    let newX = x + (Math.round(Math.random()) * 2 - 1) / 10;
-    let newY = y + (Math.round(Math.random()) * 2 - 1) / 10;
-
-    if (newX >= -2 && newX <= 2) {
-      x = newX;
-    }
-    if (newY >= -2 && newY <= 2) {
-      y = newY;
-    }
-    maxAPI.outlet(`xyz`, index + 1, x , y, 0);
-    coordsArray[index] = { x, y, ...other };
+    shuffle(coordsArray[index]).then((coord) => {
+      let { x, y, ...other } = coord;
+      maxAPI.outlet(`xyz`, index + 1, x , y, 0);
+      coordsArray[index] = { x, y, ...other };
+    })
   }
 }
 
+const shuffle = async (coord) => {
+  let { x, y, ...other } = coord;
+  let newX = x + (Math.round(Math.random()) * 2 - 1) / 50;
+  let newY = y + (Math.round(Math.random()) * 2 - 1) / 50;
+
+  if (newX >= -2 && newX <= 2) {
+    x = newX;
+  }
+  if (newY >= -2 && newY <= 2) {
+    y = newY;
+  }
+
+  return { x, y, ...other };
+} 
+
 const spinMovement = (direction) => {
   for (let index = 0; index < coordsArray.length; index++) {
-    let { x, y, angle, ...other } = coordsArray[index];
-    const radius = Math.sqrt(((x - 0) ** 2) + ((y - 0) ** 2));
-    const angleInRadians = 11.25 * Math.PI / 180;
-    angle += angleInRadians;
-    if (direction === LEFT) {
-      x = Math.cos(angle) * radius;
-      y = Math.sin(angle) * radius;
-    } else {
-      x = Math.sin(angle) * radius;
-      y = Math.cos(angle) * radius;
-    }
+    spinner(coordsArray[index], direction).then(({x,y, ...other}) => {
+      maxAPI.outlet(`xyz`, index + 1, x , y, 0);
+      coordsArray[index] = { x, y, ...other };
+    })
 
-    maxAPI.outlet(`xyz`, index + 1, x , y, 0);
-    coordsArray[index] = { x, y, angle, ...other };
   }
+}
+
+const spinner = async (coord, direction) => {
+  let { x, y, ...other } = coord;
+  let angle = 0;
+  if (direction === LEFT) {
+    angle = 3 * (Math.PI / -180);
+  } else {
+    angle = 3 * (Math.PI / 180);
+  }
+  let newX = (x * Math.cos(angle) + y * Math.sin(angle));
+  let newY = (y * Math.cos(angle) - x * Math.sin(angle));
+  x = newX;
+  y = newY;
+
+  return { x, y, ...other };
 }
 
 const pulsMovement = (...args) => {
   let vectorSize = args[0];
   for (let index = 0; index < coordsArray.length; index++) {
-    let { x, y, currentPuls, ...other } = coordsArray[index];
-    const distance = 0.05;
-    if (currentPuls <= 0) {
-      direction = UP;
-    }
-    if (currentPuls >= vectorSize) {
-      direction = DOWN;
-    }
-    if (direction === UP) {
-      currentPuls += distance;
-      x = Math.sign(x) * (Math.abs(x) + distance);
-      y = Math.sign(y) * (Math.abs(y) + distance);
-    } else if (direction === DOWN) {
-      currentPuls -= distance;
-      x = Math.sign(x) * (Math.abs(x) - distance);
-      y = Math.sign(y) * (Math.abs(y) - distance);
-    }
-    maxAPI.outlet(`xyz`, index + 1, x , y, 0);
-    coordsArray[index] = { x, y, currentPuls, ...other };
+    puls(coordsArray[index], vectorSize).then(({ x, y, currentPuls, ...other }) => {
+      maxAPI.outlet(`xyz`, index + 1, x , y, 0);
+      coordsArray[index] = { x, y, currentPuls, ...other };
+    });
   }
+}
+
+const puls = async (coord, vectorSize) => {
+  let { x, y, currentPuls, ...other } = coord;
+  const distance = 0.05;
+  if (currentPuls <= 0) {
+    direction = UP;
+  }
+  if (currentPuls >= vectorSize) {
+    direction = DOWN;
+  }
+  if (direction === UP) {
+    currentPuls += distance;
+    x = Math.sign(x) * (Math.abs(x) + distance);
+    y = Math.sign(y) * (Math.abs(y) + distance);
+  } else if (direction === DOWN) {
+    currentPuls -= distance;
+    x = Math.sign(x) * (Math.abs(x) - distance);
+    y = Math.sign(y) * (Math.abs(y) - distance);
+  }
+
+  return { x, y, currentPuls, ...other };
 }
 
 const handlers = {
